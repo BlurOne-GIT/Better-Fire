c3d70d2427397e74951cc25bf5869bc9b9da8eb2
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-02 19:35:27 -0300

    build: create project

---
+++ src/main/java/code/blurone/betterfire/BetterFire.kt
@@ -0,0 +1,14 @@
+package code.blurone.betterfire
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class BetterFire : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
